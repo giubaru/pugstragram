de7ac0286a79177ad92117b8aa708d1e249356ce
gmarinel <giuliano.marinella@telefonica.com>
2020-12-27 01:45:38 -0300

    Styles and modal

--- src/components/TimeLine.svelte
+++ src/components/TimeLine.svelte
@@ -1,9 +1,23 @@
 <script>
     import Card from './Card.svelte';
+    
+    export let posts = [];
 </script>
+
+<style>
+    .TimeLine {
+      padding: 4em 0 0 0;
+    }
+  </style>
 
 <div class="TimeLine">
     <div class="TimeLine-container">
-        <Card />
+        
+        {#each posts as post}
+            <Card {...post}/>
+        {:else}
+            <!-- TODO: Loading Component -->
+            <p>Loading...</p>
+        {/each}
     </div>
 </div>
