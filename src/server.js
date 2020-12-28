9eb516adee24a45d4965b15ef5e59b3c1a7fc241
gmarinel <giuliano.marinella@telefonica.com>
2020-12-28 01:17:09 -0300

    sapper config

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import sirv from 'sirv'
+import compression from 'compression'
+import polka from 'polka'
+import * as sapper from '@sapper/server'
+
+const { NODE_ENV } = process.env
+
+const dev = NODE_ENV === 'development'
+
+polka()
+.use(
+    compression({
+        threshold: 0
+    }),
+    sirv('static', { dev }),
+    sapper.middleweare()
+)
+.listen(POST, err => {
+    if (err) console.log(err);
+})
